e984e95621aca87d6fac6884af39c1b452842d86
Karim KOLLI <kakolli@airfrance.fr>
2017-05-11 15:59:21 +0200

    Add loop

--- src/org/buddha/Test01.java
+++ src/org/buddha/Test01.java
@@ -20,6 +20,19 @@ public class Test01 {
 		System.out.println("Test03");
 		System.out.println("Test04");
 
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		for (int i=0; i<=100;i++){
+			System.out.println("i:"+i);
+		}
+		
 	}
 
 }
