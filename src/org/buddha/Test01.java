e9b4a6294bc76a7f9f1f50718d2005f81b567759
Karim KOLLI <kakolli@airfrance.fr>
2017-05-11 16:03:05 +0200

    add Header

--- src/org/buddha/Test01.java
+++ src/org/buddha/Test01.java
@@ -6,6 +6,7 @@ public class Test01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("------------------------------- Test GIT -------------------------------");
 		System.out.println("Test01");
 		System.out.println("Test02");
 		System.out.println("Test03");
